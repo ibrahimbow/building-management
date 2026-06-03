5adc94c1ffb5ae9f081d6d63767c9cb2182b05d0
ibrahim <ibrahim.alolofi@hotmail.com>
2026-06-03 04:43:56 +0200

    add Kafka-based notification flow for announcements in production deployment

--- deployment/production/postgres/init-databases.sql
+++ deployment/production/postgres/init-databases.sql
@@ -3,4 +3,5 @@ CREATE DATABASE building_db;
 CREATE DATABASE announcement_db;
 CREATE DATABASE share_and_help_db;
 CREATE DATABASE chat_db;
+CREATE DATABASE notification_db;
 CREATE DATABASE file_db;
