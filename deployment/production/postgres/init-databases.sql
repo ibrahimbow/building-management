c2a7ed3b9643e3fb373e63fa142dcf358eba5ead
ibrahim <ibrahim.alolofi@hotmail.com>
2026-06-03 04:48:30 +0200

    complete Joritna production deployment configuration for core microservices

--- deployment/production/postgres/init-databases.sql
+++ deployment/production/postgres/init-databases.sql
@@ -5,3 +5,18 @@ CREATE DATABASE share_and_help_db;
 CREATE DATABASE chat_db;
 CREATE DATABASE notification_db;
 CREATE DATABASE file_db;
+
+
+GRANT ALL PRIVILEGES ON DATABASE auth_db TO bm_user;
+
+GRANT ALL PRIVILEGES ON DATABASE building_db TO bm_user;
+
+GRANT ALL PRIVILEGES ON DATABASE announcement_db TO bm_user;
+
+GRANT ALL PRIVILEGES ON DATABASE share_and_help_db TO bm_user;
+
+GRANT ALL PRIVILEGES ON DATABASE chat_db TO bm_user;
+
+GRANT ALL PRIVILEGES ON DATABASE notification_db TO bm_user;
+
+GRANT ALL PRIVILEGES ON DATABASE file_db TO bm_user;
